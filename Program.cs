5b57b7af6ec12a5030b86dcfbf61edbfe0904cda
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-03-25 09:36:20 +0200

    Grading Switch

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your exam result.");
+            char grade = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("Outstanding!!!");
+                    break;
+
+                case 'B':
+                    Console.WriteLine("Superior!!");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("Good!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Satisfactory.");
+                    break;
+                case 'E':
+                    Console.WriteLine("Low Pass.");
+                    break;
+                case 'F':
+                    Console.WriteLine("Failure.");
+                    break;
+                default:
+                    Console.WriteLine("What?!");
+                    break;
+            }
+        }
+    }
+}
